d19201258ca35794cccdff1a6d962a635eefadcb
Israel Aruquipa <israelaruquipaa@gmail.com>
2025-08-11 22:24:39 -0300

    Insercion de datos

--- Query_AruShop.sql
+++ Query_AruShop.sql
@@ -38,6 +38,8 @@ CREATE TABLE IF NOT EXISTS detalle_ventas(
     FOREIGN KEY(id_ventas) REFERENCES ventas (id_ventas)
 );
 
+-- INSERTAR DATOS
+
 INSERT INTO clientes(nombre, apellido, dni, correo_electronico, telefono)
 	VALUES
     ("Nahuel", "Montes", "13421471", "nahuel@gmail.com", "1145124212"),
@@ -46,18 +48,22 @@ INSERT INTO clientes(nombre, apellido, dni, correo_electronico, telefono)
     ("Valeria", "Alvarez", "33421471", "vale@gmail.com", "1145123222"),
     ("Renata", "Gilera", "43421471", "renata@gmail.com", "1155624212"),
     ("Matias", "Guatemala", "13424481", "mati@gmail.com", "1144424212");
-
+SELECT * FROM clientes; 
 INSERT INTO articulos(nombre_articulo, precio, cantidad_stock, descripcion)
 	VALUES
     ("pantalon de jean", 16000, 12, "pantalon denim"),
     ("buzo", 12000, 16, "buzo frizado 100% algodon"),
     ("remera", 8000, 6, "remera de jersey 100% algodon"),
     ("joggin", 15000, 12, "joggin frizado 100% algodon");
-
+SELECT * FROM articulos; 
 INSERT INTO ventas(id_clientes, total)
 	VALUES
     (1,60000),
     (2,75000),
     (3,15000);
-
-SELECT * FROM ventas;
+INSERT INTO detalle_ventas(id_ventas, id_articulos, cantidad, precio)
+	VALUES
+    (1, 4, 4, 15000),
+    (2, 4, 5, 15000);
+    
+SELECT * FROM detalle_ventas;
